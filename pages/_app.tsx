ff749242b0148d9c8876b3f8261226b63b54351b
brunoBrian <bruninho_brian@hotmail.com>
2022-10-07 22:28:14 -0300

    adicionando 3web

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-import '../styles/globals.css'
+import { ThirdwebProvider } from "@thirdweb-dev/react/solana";
+
 import Layout from './_layout'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <ThirdwebProvider network="devnet">
       <Layout ><Component {...pageProps} /></Layout>
+    </ThirdwebProvider>
     )
 }
 
